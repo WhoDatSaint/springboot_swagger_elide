745f34fcc681a59cc3afe142ade306feb0aebe9b
Tin Huynh <tinwhodat@gmail.com>
2017-05-02 22:23:12 -0700

    Temp commit of initial elide.io files

---
+++ src/main/java/guru/springframework/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The JPA entities that are exposed to Elide via the @Include annotation
+ */
+@Include(rootLevel=true)
+package guru.springframework.domain;
+
+import com.yahoo.elide.annotation.Include;
