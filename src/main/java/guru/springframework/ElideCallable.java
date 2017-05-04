ed82f5b19fb249bc0ef4ca623e2173faa727fec4
Tin <thuynh@salesforce.com>
2017-05-04 12:22:39 -0700

    integrated Elide 2.0.1 example

---
+++ src/main/java/guru/springframework/ElideCallable.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import com.yahoo.elide.Elide;
+
+/**
+ * We'll implement this interface as a lambda to make working with Elide easier
+ */
+public interface ElideCallable {
+    String call(final Elide elide, final String path);
+}
--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -2,10 +2,11 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
+@EnableTransactionManagement    // Elide requires transaction support, which we enable here
 public class SpringBootWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
